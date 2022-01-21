2b315d15ce9001d0e96abeb54f0e5dcc857f5946
matthewpimentel <matthewpimentel@hotmail.com>
2022-01-21 13:56:20 -0500

    role react was added

--- revbot.py
+++ revbot.py
@@ -1,12 +1,42 @@
 import random
 import discord
 
-#Add your own token
+
+    
+        
+intents = discord.Intents.default()
+intents.members = True
+
+#add your token
 TOKEN = ''
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}' .format(client))
-    
+        print('We have logged in as {0.user}' .format(client))
+
+#React role add (add the message id and emoji/roles to fit your needs)
+@client.event
+async def on_raw_reaction_add(payload):
+    messageID = 934155255925587978
+    if messageID == payload.message_id:
+        member = payload.member
+        guild = member.guild
+        
+        emoji = payload.emoji.name
+        if emoji == '🕵️‍♂️':
+            role = discord.utils.get(guild.roles, name="Detective")
+        elif emoji == '🔫':
+            role = discord.utils.get(guild.roles, name="Headshot Machine")
+        elif emoji == '🏎️':
+            role = discord.utils.get(guild.roles, name="Boost Stealer")
+        elif emoji == '👻':
+            role = discord.utils.get(guild.roles, name="Ghostbuster")
+        elif emoji == '🎲':
+            role = discord.utils.get(guild.roles, name="Table Flipper")
+        elif emoji == '🇱':
+            role = discord.utils.get(guild.roles, name="Lost Ark")
+
+        await member.add_roles(role)
+        
 client.run(TOKEN)
